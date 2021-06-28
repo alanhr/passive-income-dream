8bbc05706133471d43916c258f68357063bb769f
Alan Gomes Serafim <alanhr2@gmail.com>
2021-06-28 10:20:33 -0300

    chore(config): add lint

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  plugins: ['svelte3', '@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+    extraFileExtensions: ['.svelte'],
+  },
+  env: {
+    es6: true,
+    browser: true,
+  },
+  overrides: [
+    {
+      files: ['*.svelte'],
+      processor: 'svelte3/svelte3',
+      rules: {
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+      },
+    },
+  ],
+  settings: {
+    'svelte3/typescript': require('typescript'),
+    // ignore style tags in Svelte because of Tailwind CSS
+    // See https://github.com/sveltejs/eslint-plugin-svelte3/issues/70
+    'svelte3/ignore-styles': () => true,
+  },
+
+  ignorePatterns: ['node_modules'],
+}
---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  purge: ["./public/index.html", "./src/**/*.{svelte,ts}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      display: ["Tenor Sans", "Georgia", "serif"],
+      body: ["Inter", "system-ui", "sans-serif"],
+    },
+    colors: {
+      primary: {
+        50: "#fafafa",
+        100: "#f4f4f5",
+        200: "#e4e4e7",
+        300: "#d4d4d8",
+        400: "#a1a1aa",
+        500: "#71717a",
+        600: "#52525b",
+        700: "#3f3f46",
+        800: "#27272a",
+        900: "#18181b",
+      },
+      accent: {
+        50: "#fffbeb",
+        100: "#fef3c7",
+        200: "#fde68a",
+        300: "#fcd34d",
+        400: "#fbbf24",
+        500: "#f59e0b",
+        600: "#d97706",
+        700: "#b45309",
+        800: "#92400e",
+        900: "#78350f",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
